58761c31a142f94ebf2df44075dfe68426d65b97
sjq <1825693558@qq.com>
2026-09-18 14:01:49 +0800

    配置文件

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,7 @@ import { getToken, removeToken } from './auth'
 import router from '../router'
 
 const request = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   timeout: 15000
 })
 
